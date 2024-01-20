b51067d47c3c1a2e5a673638a5f00a86bac2d09e
Amantur <you@example.com>
2024-01-20 22:42:29 +0600

    Создана модель Пользователя

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+	internal class Gender
+	{
+		public string Name { get; }
+
+		public Gender(string name)
+		{
+			if (string.IsNullOrEmpty(name)) throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
+			Name = name;
+		}
+	}
+}                       
---
+++ CodeBlogFitness.BL/Model/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+	internal class User
+	{
+		public User(string name,
+					Gender gender,
+					DateTime birthDate,
+					double weight,
+					double height)
+		{
+			#region Проверка условий и присваивание
+			if (string.IsNullOrEmpty(name)) throw new ArgumentNullException("Имя не может быть пустым", nameof(name));
+			if (gender == null) throw new ArgumentNullException("Пол не может быть пустым", nameof(gender));
+			if (birthDate < DateTime.Parse("01.01.1900") || birthDate>= DateTime.Now) throw new ArgumentException("Неверный ввод даты рождения", nameof(birthDate));
+			if (weight< 0.0) throw new ArgumentException("Вес не может быть меньше нуля", nameof(weight));
+			if (height< 0.0) throw new ArgumentException("Рост не может быть меньше нуля", nameof(height));
+			
+			Name = name;
+			Gender = gender;
+			BirthDate = birthDate;
+			Weight = weight;
+			Height = height;
+			#endregion
+		}
+
+		public string Name { get; }
+
+		public Gender Gender { get; }
+
+		public DateTime BirthDate { get; }
+
+		public double Weight { get; set; }
+
+		public double Height { get; set; }
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+	}
+}
